1ccc80c3e25d3e324223918d5d5d0d1d4784fc10
Benji Kay <okaybenji@gmail.com>
2015-09-30 13:31:08 -0500

    accept synth configuration object

--- subpoly.js
+++ subpoly.js
@@ -5,14 +5,16 @@
  */
 
 /*
- * TODO:  Attach synth functions to its prototype.
- * TODO:  Accept configuration object for starting values
- *        including number of voices, max gain, attack, etc. 
+ * TODO: Attach synth functions to its prototype.
+ * TODO: Explore synth.cutoff.contour.
+ * TODO: Consider making synth.stereoWidth a getter/setter function which contains updateWidth.
  */
 
-var Polysynth = function(numVoices) {
+var Polysynth = function(config) {
 
   var synth = this;
+  config = config || {};
+  config.cutoff = config.cutoff || {};
   synth.voices = [];
   
   var audioCtx = new AudioContext;
@@ -25,28 +27,26 @@ var Polysynth = function(numVoices) {
   amp.connect(audioCtx.destination);
 
   //synth defaults
-  synth.maxGain = .9; //out of 1
-  synth.attack = .1; //in seconds
-  synth.decay = 0; //in seconds
-  synth.sustain = 1; //out of 1
-  synth.release = .8; //in seconds
-  synth.stereoWidth = .5; //out of 1
-
-  //low-pass filter cutoff defaults
-  synth.cutoff = filter.frequency;
-  synth.cutoff.value = 7500; //in hertz
-  synth.cutoff.maxValue = 7500; //in hertz
-  //synth.cutoff.contour = 1; //out of 1
-  synth.cutoff.attack = .1; //in seconds
-  synth.cutoff.decay = 2.5; //in seconds
-  synth.cutoff.sustain = .2; //out of 1
-
-  numVoices = numVoices || 16; //default to 16 voices
+  var numVoices       = config.numVoices    || 16;
+  var defaultWaveform = config.waveform     || 'sine';
+  var defaultPitch    = config.pitch        || 440;
   
-  function Voice(pitch, waveform) {
-    pitch = pitch || 440;
-    waveform = waveform || 'sine';
-    
+  synth.maxGain       = config.maxGain      || 0.9; //out of 1
+  synth.attack        = config.attack       || 0.1; //in seconds
+  synth.decay         = config.decay        || 0.0; //in seconds
+  synth.sustain       = config.sustain      || 1.0; //out of 1
+  synth.release       = config.release      || 0.8; //in seconds
+  synth.stereoWidth   = config.stereoWidth  || 0.5; //out of 1
+  
+  //low-pass filter cutoff defaults
+  synth.cutoff          = filter.frequency;
+  synth.cutoff.value    = config.cutoff.value     || 7500; //in hertz
+  synth.cutoff.maxValue = config.cutoff.maxValue  || 7500; //in hertz
+  synth.cutoff.attack   = config.cutoff.attack    || 0.1; //in seconds
+  synth.cutoff.decay    = config.cutoff.decay     || 2.5; //in seconds
+  synth.cutoff.sustain  = config.cutoff.sustain   || 0.2; //out of 1
+  
+  function Voice() {
     //create and connect oscillator and stereo panner for voice
     this.osc = audioCtx.createOscillator();
     this.pan = audioCtx.createPanner();
@@ -55,8 +55,8 @@ var Polysynth = function(numVoices) {
     this.pan.connect(filter);
     this.osc.start(0);
     
-    this.pitch(pitch);
-    this.waveform(waveform);
+    this.pitch(defaultPitch);
+    this.waveform(defaultWaveform);
   }
   
   Voice.prototype.pitch = function pitch(newPitch) {
@@ -76,8 +76,8 @@ var Polysynth = function(numVoices) {
     return this.osc.type;
   };
 
-  for (var i=0; i<numVoices; i++) {
-    synth.voices[i] = new Voice;
+  for (var i = 0; i < numVoices; i++) {
+    synth.voices.push(new Voice);
   }
 
   function getNow() {
@@ -89,13 +89,13 @@ var Polysynth = function(numVoices) {
 
   //update synth stereo width
   synth.updateWidth = function updateWidth() {
-    for (var i=0; i<numVoices; i++) {
-      var spread = 1/(numVoices-1);
+    synth.voices.forEach(function(voice, i) {
+      var spread = 1/(synth.voices.length - 1);
       var xPos = spread * i * synth.stereoWidth;
       var zPos = 1 - Math.abs(xPos);
-      synth.voices[i].pan.setPosition(xPos, 0, zPos);
-    }
-  }
+      voice.pan.setPosition(xPos, 0, zPos);
+    });
+  };
 
   //apply attack, decay, sustain envelope
   synth.start = function startSynth() {
@@ -105,7 +105,7 @@ var Polysynth = function(numVoices) {
     applyCutoff(now);
     amp.gain.linearRampToValueAtTime(synth.maxGain, now + atk);
     amp.gain.linearRampToValueAtTime(synth.sustain * synth.maxGain, now + atk + dec);
-  }
+  };
 
   //apply release envelope
   synth.stop = function stopSynth() {
